c1a1a243ba6513303dc4cb90fe2069276f13dae8
Carrosen <carla1.rosen@gmail.com>
2019-05-06 20:02:20 +0200

    adds a unit test for DB table: Listing model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_06_175337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.string "email"
+    t.string "phone_number"
+    t.string "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :location }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :phone_number }
+    it { is_expected.to have_db_column :availability }
+  end
 end
