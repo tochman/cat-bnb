04b32603cdafda15d07d54077e09022b1b9d1000
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-06 17:44:16 +0200

    adds first feature steps for succesfully filling in a form, but not creating it. Adds basic step defintions, and assertion steps after failing test.

---
+++ features/step_definitions/assertion_steps.rb
@@ -0,0 +1,3 @@
+Then("I should not see {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,11 @@
+Given("I visit the {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
