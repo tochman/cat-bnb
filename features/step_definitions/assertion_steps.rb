b357951d65d8c02fbbf2279e397050d42f097196
Carrosen <carla1.rosen@gmail.com>
2019-05-07 10:07:56 +0200

    adds step definition for changed feature

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,10 @@
 Then("I should see {string}") do |content|
   expect(page).to have_content content
 end
+
+Then("I should see {string} within {string} section") do |content, section|
+  id = (Listing.find_by(name: section)).id
+    within("//div[id=#{id}]") do
+      expect(page).to have_content content
+  end
+end
