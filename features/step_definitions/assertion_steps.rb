348686883243feba2baa0667de69c6c126fc6f60
zanenkn <zane.neikena@gmail.com>
2019-05-07 14:09:17 +0200

    dds a step definition for searching for string within a particular div, adjusts the index view.

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -4,7 +4,7 @@ end
 
 Then("I should see {string} within {string} section") do |content, section|
   name = Listing.find_by(name: section)
-  dom_section = "listing_#{name.id}"
+  dom_section = "#listing_#{name.id}"
   within(dom_section) do
     expect(page).to have_content content
   end
